7cfc862e2826828e04968716a2bfcc59ae3fd5a6
Sarunas <sarunas0329@gmail.com>
2025-03-03 18:09:01 +0200

    SCRUM-54 and SCRUM-59
    SCRUM-54 unfinished, just testing API calls

---
+++ Backend/Backend/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserWorkTime> User_Work_Times { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Backend/Backend/Models/UserWorkTime.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class UserWorkTime
+    {
+        [Key]
+        public int id_user_work_times { get; set; }
+        public TimeSpan start_time { get; set; }
+        public TimeSpan end_time { get; set; }
+        public string day { get; set; }
+        public int user_id { get; set; } //foreign key
+    }
+}
