92000e6f64dafc0137af7072ad188b1069fe65c0
MaestroGMT <arijus.ja11@gmail.com>
2025-03-27 17:55:35 +0200

    Kelioniu istorijos funckcionalumas

---
+++ Backend/Backend/Models/Destination.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Destination
+    {
+        [Key]
+        public int id { get; set; }
+        public string? place_name { get; set; }
+        public double? latitude { get; set; }
+        public double? longitude { get; set; }
+        public DateTime? date { get; set; }
+        public int user_id { get; set; }
+    }
+}
